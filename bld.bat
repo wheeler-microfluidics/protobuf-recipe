626f843543b577ef5cd212e722ced21f9643ef8c
Christian Fobel <christian@fobel.net>
2016-12-07 08:11:05 -0500

    Update to protobuf==3.1.0.post1

--- bld.bat
+++ bld.bat
@@ -1,3 +1,3 @@
-"%PYTHON%" -m pip install --no-cache "protobuf==3.1.0"
+"%PYTHON%" -m pip install --no-cache "protobuf==3.1.0.post1"
 if errorlevel 1 exit 1
 :: Add more build steps here, if they are necessary.
